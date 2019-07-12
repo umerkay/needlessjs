05b1d000af1eaf8cdf90290da8463b20e8e47d47
Umer Khan <umerkk164@gmail.com>
2019-07-12 22:38:35 +0300

    Not using .min.js anymore

--- examples/fractal/main.js
+++ examples/fractal/main.js
@@ -1,42 +1,48 @@
 var angle = 0;
 var length = 0;
+var direction = 2;
+var back = hueColor(0,0,0,0.1);
 
 const fractal = new Sketch(
-	{
-		container: "sketch-1",
-		frameRate: 30,
-		width: "inherit",
-		height: "250"
-	})
-	.init(function () {
-		background(0);
-	})
-	.loop(function () {
-		length = (length + 1) % 150;
-		angle = (angle + 0.02) % TWO_PI;
-		background(0,0,0,80);
-		colorMode(1);
-		stroke(loopCount() % 255, (loopCount()/2) % 150, 150);
-		translate(width / 2, height);
-		branch(length, 10);
-		stroke((loopCount() - 100) % 255, 25, 150);
-		translate(0, -height);
-		branch(-length * 2, 10);
-	});
+    {
+        frameRate: 30,
+        width: 'inherit',
+		height: 400,
+		container: 'sketch-1'
+    })
+    .init(function () {
+        background(0);
+    })
+    .loop(function () {
+        length += direction;
+        if (length == 150 || length == 0) direction *= -1;
+
+        angle = (angle + 0.02) % TWO_PI;
+
+        background(back);
+
+        colorMode(1);
+        stroke(abs(loopCount() - 100) % 255, 100, 50);
+        translate(width / 2, height);
+        branch(length, 10);
+        stroke((loopCount()) % 255, 100, 50);
+        translate(0, -height);
+        branch(-length * 2, 10);
+    });
 
 function branch(length, width) {
-	strokeWeight(width);
-	line(0, 0, 0, -length);
-	translate(0, -length);
-	if (width > 3) {
-		save();
-		rotate(angle);
-		branch(length * 0.8, width * 0.8);
-		restore();
+    strokeWeight(width);
+    line(0, 0, 0, -length);
+    translate(0, -length);
+    if (width > 3) {
+        save();
+        rotate(angle);
+        branch(length * 0.8, width * 0.8);
+        restore();
 
-		save();
-		rotate(-angle);
-		branch(length * 0.8, width * 0.8);
-		restore();
-	}
+        save();
+        rotate(-angle);
+        branch(length * 0.8, width * 0.8);
+        restore();
+    }
 }
